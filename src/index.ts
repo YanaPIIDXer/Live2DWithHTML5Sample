d6f743d24d5ad5cff91fa9c7edbf19c805e33f70
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-05-16 02:41:11 +0900

    とりあえずJSON読み込みまで

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello World");
+import axios from "axios";
+
+const modelRootPath = "./assets/LiveroiD_A-Y01/";
+const modelJson = modelRootPath + "LiveroiD_A-Y01.model3.json";
+
+window.onload = () => {
+  loadJson(modelJson);
+}
+
+/**
+ * JSON読み込み
+ * @param filePath ファイルパス
+ */
+const loadJson = async (filePath: string) => {
+  try {
+    const response = await axios.get(filePath);
+    if (response.status !== 200) { throw response; }
+    console.log(response.data);
+  } catch (error: any) {
+    alert(error.message ?? error);
+    console.error(error);
+    throw error;
+  }
+}
